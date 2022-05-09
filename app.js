709139a95e7cdc66de95a2ad964fbf42806feb48
Rei Habibi <91762839+reihabibi@users.noreply.github.com>
2022-05-09 18:29:02 +0200

    Added Animation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     $("#Data").hide();
 
-    $("#weatherBtn").click(function() {
+    $("#weatherBtn").click(function () {
         const city = $("#weatherInput").val();
         getWeatherData(city);
         $("#Data").slideDown();
@@ -13,18 +13,17 @@ $(document).ready(function () {
 async function getWeatherData(city) {
 
     const apiKey = "85413a2e953da4f357c8047545a7b90f"
-    const unit = "metric"    
+    const unit = "metric"
     const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey + "&units=" + unit + ""
 
     const responde = await fetch(apiUrl);
     const data = await responde.json();
 
-        
-
     $("#City").text(data.name);
 
     const temp = Math.round(data.main.temp)
     $("#weatherTemp").text(temp + " °C");
+
     $("#weatherDescription").text(data.weather[0].main);
 
     const temp_max = Math.round(data.main.temp_max)
@@ -32,10 +31,10 @@ async function getWeatherData(city) {
 
     const temp_min = Math.round(data.main.temp_min)
     $("#temp_min").text(temp_min + " °C");
-    
+
 
     weatherIcon = data.weather[0].icon;
-    iconUrl ="icon_svg/" + weatherIcon + ".svg";
+    iconUrl = "icon_svg/" + weatherIcon + ".svg";
     $("#WeatherIcon").attr("src", iconUrl);
     console.log(iconUrl);
 
